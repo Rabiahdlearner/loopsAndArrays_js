618f898f87b830ce8563de7d21d48c964cf8100a
rabiahdlearner <63425117+Rabiahdlearner@users.noreply.github.com>
2024-11-10 05:39:44 +0000

    Still under review

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,9 @@
 //--------------- IMPORTANT!!! ---------------
 
+const { arrayBuffer } = require("stream/consumers");
+
+//const { listenerCount } = require("events");
+
 // Use the keyboard shortcut Alt + Z (for PC) or Option + Z (for Mac) to allow word wrap on this document.  Word wrap breaks the lines so you don't have to keep scrolling left and right to read.
 
 //READ THE INFORMATION IN THE BROWSER WINDOW PANEL ON THE RIGHT BEFORE BEGINNING YOUR ASSIGNMENT!!
@@ -24,17 +28,7 @@
 
 // ---------- QUESTION 1 ----------
 // Write a function called 'repeat' that takes 1 integer parameter and, using a for or while loop, prints out the string "Hello World!" to the console the same number of times as the parameter. NOTE: for this question, since your console log should be inside your function, you only need to call/invoke the function after you write it rather than call/invoke it inside of a console.log.
-function repeat(1_integer){
-let string = "";
-for (let i=0; i < 1_integer.length) {
-    string += 1_integer;
 
-} return string;
-};
- let ar = ["Hello", "World", "!"];
- 
- console.log(repeat(ar));
- 
 // EXAMPLE "LOG":
 //    repeat(3);
 // EXAMPLE OUTPUT:
@@ -43,6 +37,14 @@ for (let i=0; i < 1_integer.length) {
 //    Hello World!
 
 // PUT YOUR CODE HERE
+
+function repeatIt(count) {
+  let string = "Hello world!";
+  str = string.repeat(count);
+  return str;
+}
+
+console.log("Q1", repeatIt(2));
 
 // ---------- QUESTION 2 ----------
 // Write a function called 'pyramidCounting' that takes 1 integer parameter and returns the sum of all of the numbers between 0 and the parameter.
@@ -54,6 +56,15 @@ for (let i=0; i < 1_integer.length) {
 // The mathematical explanation of this is 0+1+2+3+4 = 10.
 
 //PUT YOUR CODE HERE
+function pyramidCounting(par1) {
+  let num = 0;
+  for (i = 0; i < par1; i++) {
+    num += par1;
+  }
+  return num;
+}
+digit = 5;
+console.log("Q2:", pyramidCounting(digit));
 
 // ---------- QUESTION 3 ----------
 // Write a function called 'noVowels' that take a string parameter and removes vowels from that string using a loop.
@@ -65,6 +76,22 @@ for (let i=0; i < 1_integer.length) {
 
 // PUT YOUR CODE HERE
 
+function noVowel(str) {
+  vowels = ["a", "e", "i", "o", "u"];
+  for (let char of str.toLowerCase()) {
+    if (str.char === vowels.char) {
+      return (str1 = str[str.char != vowels.char]);
+    } else {
+      return str;
+    }
+  }
+}
+
+let names = "Bolutife"; //["B", "o","l", "u", "t", "i", "f", "e"]
+//"Bolutife";
+
+console.log("Q3:", noVowel(names));
+
 // ---------- QUESTION 4 ----------
 // Write a function called 'vowelCount' that takes 1 string parameter and returns the number of vowels in that string.
 
@@ -74,6 +101,17 @@ for (let i=0; i < 1_integer.length) {
 //    Q4: 6
 
 // PUT YOUR CODE HERE
+function vowelCount(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log("Q4", vowelCount(names));
 
 // ---------- QUESTION 5 ----------
 // Write a function called 'numOfOdds' that takes 1 integer parameter and returns the number of odd numbers between 0 and that number, including the number if it's odd. (Hint: Use the modulo operator)
@@ -84,6 +122,15 @@ for (let i=0; i < 1_integer.length) {
 //    Q5: 8
 
 // PUT YOUR CODE HERE
+numOfOdds = function (oneInt) {
+  let num = 0;
+  for (i = 0; (i = oneInt); i++) {
+    if (oneInt % 2 === 1) {
+      return num++;
+    }
+  }
+};
+console.log("Q5", numOfOdds(17));
 
 // ---------- QUESTION 6 ----------
 // Create two variables named 'empty' and 'full'.  Assign an empty array to the 'empty' variable and any array of strings or numbers to the 'full' variable.  Write a function called 'arrayChecker' that returns true if the array passed as a parameter is empty and false if not.  Check both the 'empty' and 'full' variables to make sure they are returning the expected values.
@@ -96,16 +143,39 @@ for (let i=0; i < 1_integer.length) {
 //    Q6 not empty: false
 
 // PUT YOUR CODE HERE
+let empty = [];
+let full = ["Tola is", 24, "years old"];
+arrayChecker = function (array) {
+  if (array.length === 0) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+console.log("Q7", arrayChecker(empty));
 
 // ---------- QUESTION 7 ----------
 // Write a function called 'getElementAt' that takes an array parameter and an integer parameter.  The function should return the element in the array at the given number index or 'null' if it doesn't exist.  Use your 'full' variable from Question 6 to test.
-
 // EXAMPLE LOGS:
 //    console.log("Q7: ", getElementAt(full, 2));
 //    console.log("Q7: ", getElementAt(full, 7));
 // EXAMPLE OUTPUT: (if the array assigned to variable 'full' is ["dream", 19, "code", 24, 180])
 //    Q7: code
 //    Q7: null
+getElementAt = function (arr, int) {
+  if (Array.isArray(arr)) {
+    return arr[int];
+  } else {
+    return "null";
+  }
+};
+
+console.log(
+  "Q8:",
+  getElementAt(full, 2),
+  getElementAt(names, 0),
+  getElementAt(empty, 0)
+);
 
 // ---------- QUESTION 8 ----------
 // Write a function called 'insertInArray' that takes an array parameter, makes a NEW array with the value '0' inserted at the second position in the NEW array.  The function should return the NEW array.  Use your 'full' variable from Question 6 to test.  NOTE: Assigning an array to a new variable does not make a copy, it's another reference to the same array.  To make a copy you can use the slice() method as in this example:
@@ -120,6 +190,16 @@ for (let i=0; i < 1_integer.length) {
 //    original array: ["dream", 19, "code", 24, 180]
 
 // PUT YOUR CODE HERE
+insertInArray = function (arr) {
+  let newArr = arr.splice(1, 0, 0);
+  return newArr;
+};
+
+console.log("Q8:", full, insertInArray(full));
+console.log("Q8:", full);
+console.log("Q8:", full.splice(1, 0, "0"));
+console.log("Q8:", insertInArray(full));
+console.log("Q8:", insertInArray(empty)); //I dont understand Q8 result
 
 // ---------- QUESTION 9 ----------
 // Write a function called 'compareArrays' that takes two Array parameters (containing numbers or strings only) and returns true if they are equal, false if not.  The purpose of this function should be to look at each element of the two arrays and compare them, returning false when they either hit two items that don't match, or returning false if the two arrays themselves are different lengths. You can test this with the 'empty' and 'full' variables from Question 6, and/or you can create a variable called 'compare' and assign it an array identical to 'full' to compare those.  To further test your function's accuracy, create a new variable called 'part' that is a partial copy of the 'full' variable and test that as well.
@@ -142,9 +222,21 @@ for (let i=0; i < 1_integer.length) {
 //    Q9 different: false
 
 // PUT YOUR CODE HERE
+compareArrays = function (arr1, arr2) {
+  if (arr1.value === arr2.value) {
+    return "true";
+  }
+  return "false";
+};
+let part = full.slice(3);
+console.log("Q9:", part);
+console.log("Q9:", empty);
+console.log("Q9:", compareArrays(full, empty));
+console.log("Q9:", compareArrays(full, part));
 
 // ---------- QUESTION 10 ----------
 // Create a variable called 'numbers' and assign it an array with at least 3 numbers as elements (example: [10, 3, 4]).  Write a function called 'calculateTotal' that takes one array parameter and loops through the array in order to return the sum of all the array elements.
+
 // STRETCH GOAL: use the `reduce()` method
 
 // EXAMPLE LOG:
@@ -153,12 +245,30 @@ for (let i=0; i < 1_integer.length) {
 //    Q10: 17
 
 // PUT YOUR CODE HERE
+let numbers = [23, 12, 222];
+calculateTotal = function (arr) {
+  let sumNum = arr.reduce((total, current) => total + current);
+  return sumNum;
+};
+
+console.log("Q10:", calculateTotal(numbers));
 
 // ---------- QUESTION 11 ----------
 // Write two functions called 'findEvens' and 'findOdds' that each take one array parameter and each returns a NEW Array of all the even or odd numbers as indicated.  NOTE: Assigning an array to a new variable does not make a copy, it's another reference to the same array.  To make a copy you can use the slice() method as in this example:
 // let newArray = originalArray.slice()
 // STRETCH GOAL: use the `filter()` method
 
+let findEvens = function (arrEvens) {
+  let evens = arrEvens.filter((num) => num % 2 === 0);
+  return evens;
+};
+
+let findOdds = function (arrOdds) {
+  let odds = arrOdds.filter((num) => num % 2 === 1);
+  return odds;
+};
+console.log("Q11:", findEvens(numbers));
+console.log("Q11:", findOdds(numbers));
 // EXAMPLE LOG:
 //    console.log("Q11 evens: ", findEvens([10,2,3,19,7,6,93]));
 //    console.log("Q11 odds: ", findOdds([10,2,3,19,7,6,93]));
@@ -172,7 +282,12 @@ for (let i=0; i < 1_integer.length) {
 // Write a function called 'makeSquares' that takes one array parameter and returns a NEW Array with the squared values of each of the numbers.  NOTE: Assigning an array to a new variable does not make a copy, it's another reference to the same array.  To make a copy you can use the slice() method as in this example:
 // let newArray = originalArray.slice()
 // STRETCH GOAL: use the `map()` method
+makeSquares = function (arr) {
+  squared = arr.map((num) => num ** 2);
+  return squared;
+};
 
+console.log("Q12:", makeSquares(numbers));
 // EXAMPLE LOG:
 //    console.log("Q12: ", makeSquares([2,5,8]));
 // EXAMPLE OUTPUT:
